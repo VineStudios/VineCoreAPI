b79b102a5166ad8744588ce5a394d6a9772e7668
Daniel <contact.vstudios@gmail.com>
2025-09-02 03:09:19 +0200

    Initial commit

---
+++ src/main/java/com/github/vinestudios/VineCoreAPI.java
@@ -0,0 +1,12 @@
+package com.github.vinestudios;
+
+
+import org.bukkit.Location;
+import org.jetbrains.annotations.Nullable;
+
+public interface VineCoreAPI {
+
+    @Nullable
+    Location getSpawnLocation();
+
+}
